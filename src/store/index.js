d4e12580638a6bb4ce862483c9e766d218b2837c
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-05-12 16:49:13 -0300

    config reactotron

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const middlewares = [];
+const composer = process.env.NODE_ENV === 'development'
+  ? compose(
+    applyMiddleware(...middlewares),
+    console.tron.createEnhancer(),
+  )
+  : applyMiddleware(...middlewares);
+const store = createStore(reducers, composer);
 
 export default store;
