157a3d8da71e425c1b7e2350019293b2ebddc75e
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-05-14 12:07:50 -0300

    using duck pattern

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,7 +1,8 @@
 import { all, takeLatest } from 'redux-saga/effects';
 
+import { Types as FavotiteTypes } from '../ducks/favorites';
 import { favoriteSaga } from './favorites';
 
 export default function* rootSaga() {
-  yield all([takeLatest('ADD_FAVORITE_REQUEST', favoriteSaga)]);
+  yield all([takeLatest(FavotiteTypes.ADD_REQUEST, favoriteSaga)]);
 }
