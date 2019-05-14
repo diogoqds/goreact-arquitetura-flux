8c7ecf42a953293b590c06e7af92add5e5bea827
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-05-14 10:24:25 -0300

    connect footer component to redux

---
+++ src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const Footer = ({ favoritesCount }) => (
+  <div>
+    <p>
+      Você tem {favoritesCount} {favoritesCount <= 1 ? 'repositório' : 'repostirórios'} favoritos
+    </p>
+  </div>
+);
+
+Footer.propTypes = {
+  favoritesCount: PropTypes.number.isRequired,
+};
+const mapStateToProps = state => ({
+  favoritesCount: state.favorites.length,
+});
+export default connect(mapStateToProps)(Footer);
